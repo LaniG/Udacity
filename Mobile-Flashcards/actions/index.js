fb2ac41fabdcbf13203234c291642cfc59ced87b
LaniG <lanigeorge3@gmail.com>
2018-01-07 15:53:36 -0400

    Missed this file
    I missed this file in the last commit

--- Mobile-Flashcards/actions/index.js
+++ Mobile-Flashcards/actions/index.js
@@ -1,24 +1,24 @@
 export const RECEIVE_DECKS = 'RECEIVE_DECKS'
-export const ADD_TITLE = 'ADD_TITLE'
+export const ADD_DECK = 'ADD_DECK'
 export const ADD_QUESTIONS = 'ADD_QUESTIONS'
 
 export function receiveDecks(decks) {
 return {
     type: RECEIVE_DECKS,
     decks,
-  }
+  };
 }
 
-export function addTitle(title) {
+export function addDeck(title) {
   return {
-    type: ADD_TITLE,
+    type: ADD_DECK,
     title,
-  }
+  };
 }
 
 export function addQuestions(params){
   return {
     type: ADD_QUESTIONS,
     params,
-  }
+  };
 }
